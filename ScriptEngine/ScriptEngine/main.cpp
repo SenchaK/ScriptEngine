45fd6b055edd479487b61a09c1ff36d8fe3909ea
SenchaK <seiyakawagishi327519@yahoo.co.jp>
2014-07-22 21:58:55 +0900

    sample追加

--- ScriptEngine/ScriptEngine/main.cpp
+++ ScriptEngine/ScriptEngine/main.cpp
@@ -1,4 +1,20 @@
-#include<cstdio>
+#include "lib\vm\sencha_vm.h"
+#include "vmplugin\VMFileSystem.h"
+#include "vmplugin\VMSystem.h"
+
+using namespace SenchaVM;
+using namespace SenchaVM::Assembly;
+class DefaultSystemCall : public VMSystemCallService {
+public :
+	virtual void onInit( VMDriver* driver ){
+		VMSystemCallService::onInit( driver );
+		this->addSystemCallService( new FileSystemLibrary() );
+		this->addSystemCallService( new VMSystem() );
+	}
+};
 
 void main(){
+	CVMMainRoutineDriver driver = OpenFromFile( "sample/FizzBuzz.txt" );
+	driver->setSystemCallService( CVMSystemCallService( new DefaultSystemCall() ) );
+	driver->executeFunction( "main" );
 }
